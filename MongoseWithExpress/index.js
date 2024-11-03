16e324e5959c239b92715e52c6e83c7e8c3c934d
NituKumari95080 <nitukumari251001@gmail.com>
2024-11-03 20:10:39 +0530

    mongoose and exprss change

--- MongoseWithExpress/index.js
+++ MongoseWithExpress/index.js
@@ -1,41 +1,82 @@
 const express = require('express');
-//path requrier
-const path= require('path')
-//requre chat models
-const Chat=require(`./models/chat`)
-//mongoose
+// Path and Mongoose requirements
+const path = require('path');
 const mongoose = require('mongoose');
-main()
- .then((res)=>{
-    console.log(`Connection succeful`)
- })
- .catch(err => console.log(err));
+const Chat = require('./models/chat');
+const exp = require('constants');
+const methodOverride= require('method-override');
 
+const app = express();
+app.use(express.urlencoded({extended:true}))
+const PORT = 8000;
+
+// Connect to MongoDB
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
-
+  console.log('Connection successful');
 }
 
-const app=express();
-app.set(`views`,path.join(__dirname,`views`));
-app.set(`view engine`,`ejs`)
-PORT=8000;
+main().catch(err => console.log(err));
 
-app.get(`/`,(req,res)=>{
-    res.send(`root is working `)
+// Set EJS as the view engine and define views directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,`public`)))
+app.use(methodOverride(`_method`))
+
+// Root route
+
+
+// Route to render chat messages
+app.get('/chats', async (req, res) => {
+ 
+    let chats = await Chat.find();
+    res.render('chats', { chats });
+  
+});
+//nrew route
+app.get('/chats/new', async (req, res) => {
+ res.render('new.ejs');
+  
+});
+//create route
+app.post(`/chats`,(req,res)=>{
+  let {from,to,msg}=req.body;
+  let newChat=new Chat({
+    from:from,
+    to:to,
+    msg:msg,
+    created_at:new Date()
+  })
+  newChat.save().then((res)=>{
+    console.log(`Chat was saved`)
+  }).catch((err)=>{
+    console.log(err)
+  })
+  res.redirect(`/chats`)
+})
+//edit msg
+app.get(`chats/:id/edit`,async(req,res)=>{
+  //id nikanle ke liy
+  let {id}=req.params;
+  let chat= await Chat.findById(id);
+  res.render(`edit.ejs`,{chat})
+})
+//UPDATE ROUTE
+app.put(`chats/:id`,async(req,res)=>{
+  //id nikanle ke liy
+  let {id}=req.params;
+  let {msg:newMsg}=req.body;
+  let updatedChat= await Chat.findByAndUpdate(
+    id,
+    {msg:newMsg},
+    {runValidators:true,new:ture}
+  )
+  console.log(updatedChat)
+  res.render(`/chats`)
 })
 
-//create route for chats
-app.get(`/chats`,async(req,res)=>{
-    let chats= await Chat.find();
-    console.log(chats)
-    res.send(`working`)
-})
-
-app.get(`/`,(req,res)=>{
-    res.render(`home.ejs`)
-})
-
-app.listen(PORT,(req,res)=>{
-    console.log(`Server is running..${PORT}`)
-})
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
